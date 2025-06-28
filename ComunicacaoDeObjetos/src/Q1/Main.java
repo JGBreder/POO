e261c86fac956be6654385d0ca7d85960e3ddbbd
JGBreder <jgtavaresbreder@gmail.com>
2025-06-28 18:29:18 -0300

    Add files via upload

---
+++ ComunicacaoDeObjetos/src/Q1/Main.java
@@ -0,0 +1,27 @@
+package Q1;
+
+public class Main {
+    public static void main (String args[]){
+        Circulo circ = new Circulo (5,3,10);
+        System.out.println("Identidade: " + circ);
+
+
+        Grafico g1 = new Grafico(true, '#');
+        g1.plota(1);
+        g1.plota(2);g1.plota(4);
+        g1.plota(8);
+
+
+        Grafico g2 = new Grafico(false, '*');
+        g2.plota(4);
+        g2.plota(5);
+        g2.plota(10);
+
+
+        Potencia p = new Potencia(1,7);
+        p.conecta(g1);
+        p.apresenta();
+        p.conecta(g2);
+        p.apresenta();
+    }
+}
